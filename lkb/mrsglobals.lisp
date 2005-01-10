525790847d4fd068aa4ce0da14e61c74ddd292e0
Francis Bond <bond@ieee.org>
2005-01-10 13:58:56 +0000

    added ppoi, generic_num-cl-lex, cleaned up tani/tanni and koto hiragana/kanji

--- lkb/mrsglobals.lisp
+++ lkb/mrsglobals.lisp
@@ -1,3 +1,4 @@
+;;; -*- Mode: LISP; Syntax: Common-Lisp; Package: LKB -*-
 ;;; Copyright (c) 1998-2003
 ;;;   John Carroll, Ann Copestake, Robert Malouf, Stephan Oepen;
 ;;;   see `licence.txt' for conditions.
