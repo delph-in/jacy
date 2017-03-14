a4a7d14631d785f03dd1ada2c21206cb21b7fa1f
Francis Bond <bond@ieee.org>
2017-03-14 11:30:27 -0700

    bash script for simple parsing

--- utils/runme.bash
+++ utils/runme.bash
@@ -1,11 +1,20 @@
+###
+### script for parsing with ace/art
+###
+
 suffix=`date -Iminutes` 
-jacy=/home/bond/svn/jacy-2016-11-08
+jacy=/home/bond/svn/jacy
 outdir=/home/bond/jacy
 
-# parse a new profile
-prof="tc-006"
-mkdir -p ${outdir}/${prof}
-mkprof -s ${jacy}/tsdb/skeletons/tanaka/tc-006 ${outdir}/${prof}/${suffix}
-art -a "ace -g ${jacy}/jacy.dat -n 5"  ${outdir}/${prof}/${suffix}
 
-python3 check.py ${outdir}/${prof}/${suffix} > ${outdir}/${prof}/${suffix}/check
+profs="mrs tc-006"
+# parse a new profile
+for prof in ${profs}
+do
+    echo Parsing ${prof}
+    mkdir -p ${outdir}/${prof}
+    mkprof -s ${jacy}/tsdb/skeletons/tanaka/tc-006 ${outdir}/${prof}/${suffix}
+    art -a "ace -g ${jacy}/jacy.dat -n 5"  ${outdir}/${prof}/${suffix}
+
+    python3 check.py ${outdir}/${prof}/${suffix} > ${outdir}/${prof}/${suffix}/check
+done
